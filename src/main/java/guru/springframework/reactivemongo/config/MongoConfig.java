65a2aa45a6a06a3333f136a491ff80eccdc965fb
John Thompson <john@springframework.guru>
2022-12-30 15:42:40 -0500

    adding mongo credentials

--- src/main/java/guru/springframework/reactivemongo/config/MongoConfig.java
+++ src/main/java/guru/springframework/reactivemongo/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package guru.springframework.reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -21,4 +26,26 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     protected String getDatabaseName() {
         return "sfg";
     }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts((singletonList(
+                            new ServerAddress("127.0.0.1", 27017)
+                    )));
+                });
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
