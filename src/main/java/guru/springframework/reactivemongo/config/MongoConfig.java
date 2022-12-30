5f15efc734da8aabace72cfc5ec16f932d1dfee4
John Thompson <john@springframework.guru>
2022-12-30 15:33:33 -0500

    adding mongo config

---
+++ src/main/java/guru/springframework/reactivemongo/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.reactivemongo.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Configuration
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "sfg";
+    }
+}
