b92b092db3f1b8e9c16df80192a0ab7f7c544333
John Thompson <john@springframework.guru>
2022-12-30 14:11:50 -0500

    Initial Project add

---
+++ src/main/java/guru/springframework/reactivemongo/Spring6ReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.reactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveMongoApplication.class, args);
+    }
+
+}
