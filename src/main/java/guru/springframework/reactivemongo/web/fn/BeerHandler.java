994c55c799baefa997489b7122390b52d8b20147
John Thompson <john@springframework.guru>
2023-01-01 11:41:28 -0500

    adding webflux handler

---
+++ src/main/java/guru/springframework/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,23 @@
+package guru.springframework.reactivemongo.web.fn;
+
+import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request){
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
