1b7028f9aeed6f76a323cd2ecb9eea0a480f0b0a
John Thompson <john@springframework.guru>
2024-05-20 16:20:27 -0400

    updating spring security config

--- src/main/java/guru/springframework/reactivemongo/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/reactivemongo/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.reactivemongo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -15,11 +16,9 @@ public class SpringSecConfig {
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
-        http.csrf().disable()
-                .authorizeExchange()
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer().jwt();
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
     }
